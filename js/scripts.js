7e7c09d1500196b35ee6c4ff97ce71b1a602d36b
sabirhassan <sabirbk06@gmail.com>
2020-09-09 12:26:24 +0500

    week 4 npm scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+
+$(document).ready(function(){
+    $("#loginbtn").click(function(){
+        $("#loginModal").modal('show');
+    });
+
+    $("#reservationbtn").click(function(){
+        $("#ReservationModal").modal('show');
+    });
+});
+
